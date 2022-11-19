0d315eec097815dba758c6dbbf35aaa7aa7447af
Tran IT <tranit1209@gmail.com>
2022-11-20 00:00:01 +0700

    Logger for dev mode

--- src/utils/Logger.ts
+++ src/utils/Logger.ts
@@ -1,35 +1,40 @@
-import winston from "winston";
-import {PlatformService} from "terminus-core";
-import DevConstants from "../services/dev-constants";
+// eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+import winston from 'winston'
+import { PlatformService } from 'terminus-core'
+import DevConstants from '../services/dev-constants'
 
 const path = require('path')
 export default class Logger {
-    platform;
-    logger;
+    platform
+    logger
 
-    constructor(platform: PlatformService) {
+    constructor (platform: PlatformService) {
         this.platform = platform
         const date = new Date()
-        const loggerFile = path.dirname(this.platform.getConfigPath()) + '/tabby-sync/' + ([date.getDate(), date.getMonth() + 1, date.getFullYear()].join('-')) + '.log'
+        const loggerFile = path.dirname(this.platform.getConfigPath()) + '/tabby-sync/' + [date.getDate(), date.getMonth() + 1, date.getFullYear()].join('-') + '.log'
 
         this.logger  = winston.createLogger({
             transports: [
                 new winston.transports.Console(),
-                new winston.transports.File({ filename: loggerFile })
-            ]
+                new winston.transports.File({ filename: loggerFile }),
+            ],
+            silent: DevConstants.ENABLE_DEBUG,
         })
     }
 
-    log (content, level = 'info') {
+    // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+    log (content, level = 'info'): void {
         const time = new Date()
         if (DevConstants.ENABLE_DEBUG) {
-            console.log(time.toString() + ' ', content);
+            console.log(time.toString() + ' ', content)
         }
 
-        this.logger.log({
-            level: level,
-            time: time.toLocaleString(),
-            message: content
-        });
+        if (DevConstants.ENABLE_DEBUG) {
+            this.logger.log({
+                level: level,
+                time: time.toLocaleString(),
+                message: content,
+            })
+        }
     }
 }
